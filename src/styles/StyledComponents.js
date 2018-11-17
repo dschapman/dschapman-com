102a74080b771bcd5fdadbebf332d074f9f1b4d4
Unknown <scriptor.deo@gmail.com>
2018-11-17 10:42:20 -0500

    Added case for mobile sized screens in Content component

--- src/styles/StyledComponents.js
+++ src/styles/StyledComponents.js
@@ -5,7 +5,7 @@ const Body = styled.div(
 )
 
 const Content = styled.div(
-  tw`flex-1 px-25 xs:px-4 sm:px-10 md:px-15 lg:px-20 overflow-y-auto scrolling-touch scrolling-auto mx-auto 2xl:max-w-2xl xl:max-w-xl lg:max-w-lg md:max-w-md sm:max-w-sm`,
+  tw`flex-1 px-25 xs:px-4 sm:px-10 md:px-15 lg:px-20 overflow-y-auto scrolling-touch scrolling-auto mx-auto 2xl:max-w-2xl xl:max-w-xl lg:max-w-lg md:max-w-md sm:max-w-sm xs:max-w-xs`,
 )
 
 const Div = styled.div(
