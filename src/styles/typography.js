f1a256274e60a06da87afa81fb0a2fe06c29409c
Daniel Chapman <danielstephenchapman@gmail.com>
2018-12-29 22:08:50 -0800

    LaTeX Guide
    - Started Draft of LaTeX poetry guide
    - Updated pre css style on typography.js

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -51,6 +51,10 @@ const typography = new Typography({
         'blockquote cite:before': {
           content: '"— "',
         },
+        'pre': {
+            boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+            padding: '1em'
+        }
     }),
 
 })
