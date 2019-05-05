31de3fad2cb2facca97f2ed2dbcea8e2c4b59595
Daniel <Scriptor.Deo@gmail.com>
2019-05-05 17:31:13 -0400

    Update Season of Thought Copy

--- src/pages/poetry/seasons-of-thought.js
+++ src/pages/poetry/seasons-of-thought.js
@@ -6,7 +6,7 @@ import EmailForm from '../../components/EmailForm'
 import cover from './seasons-of-thought-cover.png'
 import card from './seasons-of-thought-card.png'
 import {jsx,css} from "@emotion/core"
-import Poem from "../../components/Poem"
+import {mq} from "../../styles/styles"
 
 
 const Layout = () => {
@@ -17,10 +17,10 @@ const Layout = () => {
             
                     <H2 css={css(tw`xs:text-center sm:text-center md:text-center lg:text-left xl:text-left`)}>Seasons of Thought</H2>
                     <div css={css(tw` flex xs:flex-wrap sm:flex-wrap md:flex-wrap lg:flex-no-wrap xl:flex-no-wrap`)}>
-                    <img css={css(`padding-right: 2rem; width:auto; max-height:23rem; height:auto;`)} src={cover}/>
+                    <img css={css(`width:auto; max-height:23rem; height:auto; ${mq[0]}{padding-right: 0;};${mq[2]}{padding-right: 2rem;};`)} src={cover}/>
                     <div css={css(``)}>
-                        <p>Seasons of Thought is a collection of poems that traces its themes through the seasons of the year.</p>
-                        <p>Make sure to sign up for the email list and get notified when the book releases on Amazon this June!</p>
+                        <p>Seasons of Thought is a collection of poetry that traces its themes across the changing seasons. The poems in the collection use the passage of time as a canvas to explore nature, family, childhood, faith, and other aspects of the human condition. Seasons of Thought is D.S. Chapman’s first collection of poetry and will be available for purchase from Amazon in June.</p>
+                        <p>Sign up for D.S. Chapman's email list to be notified when this and other projects release.</p>
                     </div>
                     </div>
 
