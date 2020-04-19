6f249973d65d346d24fb2efffb4ea4b717d3f4e1
Daniel <Scriptor.Deo@gmail.com>
2020-04-19 11:37:04 -0400

    fix error that caused blog posts to show up twice

--- src/gatsby-theme-blog-core/components/post.js
+++ src/gatsby-theme-blog-core/components/post.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-import SEO from '../../components/seo'
 
 import Layout from '../../components/post-layout'
-import PostTitle from '../../components/post-title'
-import { useStaticQuery, graphql } from 'gatsby'
 
 export default ({ data, pageContext }) => {
   return (
     <Layout title={data.blogPost.title} description={data.blogPost.excerpt}>
-      <PostTitle>{data.blogPost.title}</PostTitle>
       <MDXRenderer>{data.blogPost.body}</MDXRenderer>
     </Layout>
   )
