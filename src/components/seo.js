200823f60d6d5ea488740d9e19d0db2d8ab72db4
Daniel <Scriptor.Deo@gmail.com>
2020-04-19 11:28:25 -0400

    Corrected variable names in seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -23,14 +23,14 @@ export default ({ title, description, timeToRead }) => {
   const ogImage = `https://quizzical-liskov-0bc7e1.netlify.app/opengraph?title=${seoTitle}&author=${author}&description=${seoDescription}&timeToRead=${timeToRead}&v=0.0.4`
   return (
     <Helmet title={seoTitle} description={seoDescription}>
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
+      <meta property="og:title" content={seoTitle} />
+      <meta property="og:description" content={seoDescription} />
       <meta property="og:image" content={ogImage} />
       <link rel="stylesheet" href="https://use.typekit.net/osf8fyt.css"></link>
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={siteMetadata.twitter} />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
+      <meta name="twitter:title" content={seoTitle} />
+      <meta name="twitter:description" content={seoDescription} />
       <meta name="twitter:image" content={ogImage} />
     </Helmet>
   )
