d1db14c8fe72c4904d241b2fbc09423d8dde6cc9
Daniel Chapman <Scriptor.Deo@gmail.com>
2020-05-12 08:46:09 -0400

    added location to tag pages

--- src/gatsby-theme-blog-tags/components/tag.js
+++ src/gatsby-theme-blog-tags/components/tag.js
@@ -4,8 +4,12 @@ import SEO from '../../components/seo'
 import Layout from '../../components/layout'
 import PostList from '../../components/post-list'
 
-export default ({ data, pageContext }) => (
-  <Layout title={'Articles on ' + pageContext.tag} titleTagName="h1" type="📚">
+export default ({ data, pageContext, location }) => (
+  <Layout
+    title={'Articles on ' + pageContext.tag}
+    titleTagName="h1"
+    type="📚"
+    location={location}>
     <PostList posts={data.allBlogPost.edges} />
   </Layout>
 )
--- src/gatsby-theme-blog-tags/components/tags.js
+++ src/gatsby-theme-blog-tags/components/tags.js
@@ -4,8 +4,12 @@ import SEO from '../../components/seo'
 import Layout from '../../components/layout'
 import TagList from '../../components/tags/list'
 
-export default ({ data }) => (
-  <Layout title="Articles by Tags" titleTagName="h1" type="📚">
+export default ({ data, location }) => (
+  <Layout
+    title="Articles by Tags"
+    titleTagName="h1"
+    type="📚"
+    location={location}>
     <TagList tags={data.allBlogPost.group} />
   </Layout>
 )
